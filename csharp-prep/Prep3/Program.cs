6cc790f87c1958d9aeb7f67af7f60f5715fd7429
Alderesu <kylealdrich.moran@gmail.com>
2024-09-18 18:33:19 +0800

    Assignment Finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            // For Part 3, where we use a random number
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+            int numGuesses = 0;
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                numGuesses++;
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+
+            Console.WriteLine($"It took you {numGuesses} guesses to get it right!");
+
+            Console.Write("Do you want to play again? (yes/no) ");
+            string response = Console.ReadLine();
+
+            if (response.ToLower() != "yes")
+            {
+                playAgain = false;
+            }
+        }
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // Display welcome message
+        DisplayWelcomeMessage();
+
+        // Get user name
+        string userName = PromptUserName();
+
+        // Get user number
+        int userNumber = PromptUserNumber();
+
+        // Calculate square of number
+        int squaredNumber = SquareNumber(userNumber);
+
+        // Display result
+        DisplayResult(userName, squaredNumber);
+    }
+
+    // Display welcome message
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    // Prompt user for name
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    // Prompt user for number
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    // Calculate square of number
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    // Display result
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
