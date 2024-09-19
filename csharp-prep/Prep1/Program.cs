76e1db28b8c94ff6fdd305493b2c184412f23911
Alderesu <kylealdrich.moran@gmail.com>
2024-09-19 14:43:28 +0800

    Assignment Finished

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,10 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name? ");
-        string firstName = Console.ReadLine();
-        Console.Write("What is your last name? ");
-        string lastName = Console.ReadLine();
+        string first = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
